f534a190e1bfb3cc1e7f105dcfb64b76cdb55df3
19anti91 <19anti91@gmail.com>
2018-10-17 00:09:24 -0400

    Added pet service map impl. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Anti1991 on 10/16/2018
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
